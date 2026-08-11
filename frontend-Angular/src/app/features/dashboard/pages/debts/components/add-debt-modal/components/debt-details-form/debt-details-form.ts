78e3bf9166ffe958f8b0ef730274bc609627938c
Fabian1803 <fabianriveraabian3@gmail.com>
2026-08-11 16:03:56 -0500

    feat: add AddDebtModal component and debt details form for registering and editing debts

--- frontend-Angular/src/app/features/dashboard/pages/debts/components/add-debt-modal/components/debt-details-form/debt-details-form.ts
+++ frontend-Angular/src/app/features/dashboard/pages/debts/components/add-debt-modal/components/debt-details-form/debt-details-form.ts
@@ -2,7 +2,6 @@ import { Component, model } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FormInputComponent } from '../form-controls/form-input';
-import { MonthPickerInputComponent } from '../form-controls/month-picker-input';
 
 export type PaymentMode = 'fixed_term' | 'free_payment';
 
@@ -12,25 +11,21 @@ export type PaymentMode = 'fixed_term' | 'free_payment';
   imports: [
     CommonModule,
     FormsModule,
-    FormInputComponent,
-    MonthPickerInputComponent
+    FormInputComponent
   ],
   templateUrl: './debt-details-form.html',
 })
 export class DebtDetailsFormComponent {
-  category = model<string>('Préstamo Personal');
+  category = model<string>('Crédito personal');
   totalAmount = model<number>(6000);
-  paymentMode = model<PaymentMode>('fixed_term');
   fixedTermMonths = model<number>(12);
-  freePaymentMonthlyQuota = model<number>(150);
-  startDate = model<string>('2026-07');
+  startDate = model<string>('2026-07-18');
 
   categoryOptions = [
-    'Préstamo Personal',
-    'Tarjeta de Crédito',
-    'Crédito Vehicular',
-    'Crédito Hipotecario',
-    'Préstamo Familiar / Informal',
-    'Otro'
+    'Crédito de vivienda',
+    'Crédito educativo',
+    'Tarjeta de crédito',
+    'Crédito vehiculo',
+    'Crédito personal'
   ];
 }
